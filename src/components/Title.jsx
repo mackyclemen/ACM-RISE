530e824108bcb5a8278f1c79cdea8eacc57dd650
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-11 22:27:38 +0800

    UPDATE: Title font colors

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Title({ title, description = '' }) {
   return (
     <>
-      <h1 className="text-white font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold">
+      <h1 className="font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold">
         {title}
       </h1>
       {description && <p>{description}</p>}
