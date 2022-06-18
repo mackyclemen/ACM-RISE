76508308b183c489201d9b33aab2ecd1d9cf0f49
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-19 03:19:31 +0800

    UPDATE: FAQ page

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import '../style/accordion.css'
+
+const Accordion = ({ title, children }) => {
+    const [isOpen, setOpen] = useState(false);
+    return (
+      <div className="accordion-wrapper">
+        <div
+          className={`accordion-title bg-gradient-to-r from-riseDarkBlue via-riseLightBlue to-riseDarkBlue ${isOpen ? "open" : ""}`}
+          onClick={() => setOpen(!isOpen)}
+          >
+          {title}
+        </div>
+        <div className={`accordion-item ${!isOpen ? "collapsed" : ""}`}>
+          <div className="accordion-content">{children}</div>
+        </div>
+      </div>
+    );
+  };
+
+  export default Accordion;
