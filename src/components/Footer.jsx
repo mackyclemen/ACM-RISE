a6407f7bfea0d1c87efd7cc07c6e8d9dfb8f3a07
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-19 19:11:06 +0800

    UPDATE: add additional social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,30 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-black py-12 text-center flex flex-col justify-center items-center gap-4">
-        <a href="https://www.facebook.com/feutechACM"><img src="/images/facebook-logo.png" alt="facebook logo" /></a>
+      <div className="flex gap-4">
+        <a href="https://www.facebook.com/feutechACM">
+          <img
+            className="w-9"
+            src="/images/facebook-logo.png"
+            alt="facebook logo"
+          />
+        </a>
+        <a href="https://www.instagram.com/feutechacm">
+          <img
+            className="w-9"
+            src="/images/instagram-logo.svg"
+            alt="instagram logo"
+          />
+        </a>
+        <a href="https://www.twitter.com/feutechACM">
+          <img
+            className="w-9"
+            src="/images/twitter-logo.svg"
+            alt="twitter logo"
+          />
+        </a>
+      </div>
+
       <div>
         <p>Copyright 2022 – FEUTECH ACM.</p>
         <p>
