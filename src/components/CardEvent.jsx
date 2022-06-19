fbf437c923a04e56d57ea4998facbd7a0144f2a5
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-19 15:27:26 +0800

    UPDATE: finished layout of events

--- src/components/CardEvent.jsx
+++ src/components/CardEvent.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-function CardEvent({title, description}) {
+function CardEvent({ title, dateText, eventLists, isLast }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 md:w-1/3 md:p-8">
-      <img
-        src="https://placehold.jp/150x150.png"
-        className="w-[150px] rounded-3xl"
-        alt="event image"
-      />
-      <h4 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold">{title}</h4>
-      <p>{description}</p>
+    <div className="flex gap-12">
+      <h4 className="min-w-fit mt-4 text-white font-bold hidden lg:block">{title}</h4>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center items-center h-16 w-16 rounded-full bg-gradient-to-r from-riseDarkGold to-riseLightGold">
+          <p className="text-black">{dateText}</p>
+        </div>
+        <div className={`flex-grow w-1 bg-gradient-to-r from-riseDarkGold to-riseLightGold ${isLast && 'hidden'}`}></div>
+      </div>
+      <ul className="list-disc mt-4 mb-8 text-left">
+        {eventLists.map((eventList, ind) => (
+          <li className="mb-4 text-lg" key={ind + eventList}>
+            {eventList}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
