a3b4844acad1489219b7bbc102abeeaaf85cc530
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-19 16:51:28 +0800

    UPDATE: Font style in events page

--- src/components/CardEvent.jsx
+++ src/components/CardEvent.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function CardEvent({ title, dateText, eventLists, isLast }) {
   return (
     <div className="flex gap-12">
-      <h4 className="min-w-fit mt-4 text-white font-bold hidden lg:block">{title}</h4>
+      <h4 className="min-w-fit mt-4 text-white text-xl font-bold hidden lg:block">{title}</h4>
       <div className="flex flex-col items-center">
         <div className="flex justify-center items-center h-16 w-16 rounded-full bg-gradient-to-r from-riseDarkGold to-riseLightGold">
-          <p className="text-black">{dateText}</p>
+          <p className="text-black text-sm">{dateText}</p>
         </div>
         <div className={`flex-grow w-1 bg-gradient-to-r from-riseDarkGold to-riseLightGold ${isLast && 'hidden'}`}></div>
       </div>
