d07c6401d1d624b907fd2cbada385b0bd24c13c9
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-19 17:37:57 +0800

    UPDATE: Events id

--- src/components/CardEvent.jsx
+++ src/components/CardEvent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function CardEvent({ title, dateText, eventLists, isLast }) {
+function CardEvent({ title, id, dateText, eventLists, isLast }) {
   return (
-    <div className="flex gap-12">
+    <div className="flex gap-12" id={id}>
       <h4 className="min-w-fit mt-4 text-white text-xl font-bold hidden lg:block">{title}</h4>
       <div className="flex flex-col items-center">
         <div className="flex justify-center items-center h-16 w-16 rounded-full bg-gradient-to-r from-riseDarkGold to-riseLightGold">
