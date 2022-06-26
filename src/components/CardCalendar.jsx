1d9c563b4949c7e7786beaa4b39505779bf7e059
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-26 14:35:48 +0800

    UPDATE: general calendar

--- src/components/CardCalendar.jsx
+++ src/components/CardCalendar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-function CardEvent({title, description}) {
+function CardEvent({title, date, description, imgSrc}) {
   return (
     <div className="flex flex-col justify-center items-center gap-4 md:w-1/3 md:p-8">
       <img
-        src="https://placehold.jp/150x150.png"
+        src={imgSrc}
         className="w-[150px] rounded-3xl"
         alt="event image"
       />
-      <h4 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold">{title}</h4>
-      <p className='text-gray-300 text-xl'>{description}</p>
+      <div className="text-center text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold">
+        <h4 className="text-3xl font-bold">{title}</h4>
+        <span className="text-base font-semibold">{date}</span>
+      </div>
+      <p className='text-gray-300 text-xl text-center max-w-[250px]'>{description}</p>
     </div>
   );
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Title({ title, description = '' }) {
   return (
     <div className="flex flex-col gap-4 lg:w-2/3">
-      <h1 className="font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold">
+      <h1 className="font-bold text-6xl text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold leading-tight">
         {title}
       </h1>
       {description && <p className="text-gray-300 text-xl">{description}</p>}
