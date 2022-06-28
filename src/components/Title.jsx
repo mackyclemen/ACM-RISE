fe70e8aa0d380a2acdc4f5fbebe5cb287b1b13bf
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-06-28 20:16:20 +0800

    UPDATE: implementation of Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function Title({ title, description = '' }) {
+function Title({ title, children = null}) {
   return (
-    <div className="flex flex-col gap-4 2xl:w-2/3">
+    <div className="flex flex-col gap-4 items-center">
       <h1 className="font-bold text-6xl text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold leading-tight">
         {title}
       </h1>
-      {description && <p className="text-gray-300 text-xl">{description}</p>}
+      <div className="flex flex-col gap-4 2xl:w-2/3 text-gray-300 text-xl">
+        {children}
+      </div>
     </div>
   );
 }
