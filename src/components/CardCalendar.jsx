908600148fc90aa006c86a5ac76fac7dc7c4260d
Simperfy <28738855+Simperfy@users.noreply.github.com>
2022-07-02 12:21:44 +0800

    UPDATE: add links to events card in homepage

--- src/components/CardCalendar.jsx
+++ src/components/CardCalendar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function CardEvent({title, date, description, imgSrc}) {
+function CardEvent({title, date, description, imgSrc, link="#"}) {
   return (
     <div className="flex flex-col justify-center items-center gap-4 md:w-1/3 md:p-8">
       <img
@@ -9,7 +9,7 @@ function CardEvent({title, date, description, imgSrc}) {
         alt="event image"
       />
       <div className="text-center text-transparent bg-clip-text bg-gradient-to-r from-riseDarkGold to-riseLightGold">
-        <h4 className="text-3xl font-bold">{title}</h4>
+        <a href={link} className="block text-3xl font-bold hover:underline underline-offset-4  decoration-riseLightGold">{title}</a>
         <span className="text-base font-semibold">{date}</span>
       </div>
       <p className='text-gray-300 text-xl text-center max-w-[250px]'>{description}</p>
